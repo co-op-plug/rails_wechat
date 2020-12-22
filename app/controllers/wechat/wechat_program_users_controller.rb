f06cdc4d08b5a4178f85a167e510c4d28a5ab161
jack <qinshenyong@socialdatamax.com>
2020-12-22 12:21:33 +0800

    fix

--- app/controllers/wechat/wechat_program_users_controller.rb
+++ app/controllers/wechat/wechat_program_users_controller.rb
@@ -38,7 +38,7 @@ class Wechat::WechatProgramUsersController < Wechat::BaseController
       @account = Account.find_by(identity: phone_number) || Account.create_with_identity(phone_number)
 
       if @account.user.nil?
-        wechat_user = WechatUser.find_by unionid: wechat_program_user.unionid
+        wechat_user = WechatUser.find_by unionid: @wechat_program_user.unionid
         origin_user = wechat_user.user
         @wechat_program_user.user = origin_user
         @wechat_program_user.save
